532f3bceb26d50a56a0f3b0bdab96e7e5e237238
DKGE <koenmoorman@dkge.nl>
2021-11-10 15:47:35 +0100

    Update search.sh

--- search.sh
+++ search.sh
@@ -3,5 +3,5 @@
 # Date:         10.11.2021
 # Version:      1.0.0
 
-find / -type f -exec grep -l "You have been blocked for too many failed login attempts or you do not have permission to access the system." {} \; >> /volume1/home/dkge/search.txt
+find / -type f -exec grep -l "You have been blocked for too many failed login attempts or you do not have permission to access the system." {} \; >> search.txt
 echo -e "\033[0;32mSearch Complete\033[0m"
