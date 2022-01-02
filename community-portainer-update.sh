aac13069a51eacc6bc34fe4cbe455e94a8a5241c
DKGE <koenmoorman@dkge.nl>
2022-01-02 06:00:21 +0100

    General update

--- community-portainer-update.sh
+++ community-portainer-update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Purpose:      Update portainer enterprise edition
+# Purpose:      Update portainer community edition
 # Date:         02.01.2022 (DD.MM.YYYY)
 # Version:      1.0.0
 # Tested on:    Debian 11 (Bullseye)
 #==============================================================================
 docker stop portainer
 docker rm portainer
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always --pull=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
--- portainer-clean.sh
+++ portainer-clean.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Purpose:      Update portainer enterprise edition
+# Purpose:      Remove old portainer enterprise edition container including data and create new one
 # Date:         02.01.2022 (DD.MM.YYYY)
 # Version:      1.0.0
 # Tested on:    Debian 11 (Bullseye)
 #==============================================================================
 docker stop portainer
 docker rm portainer
+docker system prune -a --volumes -f
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always --pull=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee
