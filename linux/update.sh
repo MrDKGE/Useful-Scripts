e7bfe2aa724dfb35c5fd917e9cc86a065df91781
MrDKGE <koenmoorman@dkge.nl>
2022-12-12 07:21:16 +0100

    cleanup

--- linux/update.sh
+++ linux/update.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # Purpose:      Updating software with custom messages.
-# Version:      1.0.1
+# Version:      1.0.2
 # Tested on:    Debian 11 (Bullseye)
 #==============================================================================
-echo -e "\033[0;32mFetching list of available updates...\033[0m"
-sudo apt update
-echo -e "\033[0;32mUpgrading current packages...\033[0m"
-sudo apt upgrade -y
+echo -e "\033[0;32mFetching list of available updates and upgrading current packages\033[0m"
+sudo apt update && sudo apt full-upgrade -y
 echo -e "\033[0;32mRemoving orphaned packages/dependencies...\033[0m"
 sudo apt autoremove -y
 echo -e "\033[0;32mClearing sudo apt cache...\033[0m"
