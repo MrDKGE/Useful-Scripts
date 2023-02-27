5c33514342e7f7c09aff4d276972a0b4e894570f
DKGE <koenmoorman@dkge.nl>
2023-02-27 15:55:36 +0100

    Added pull to portainer

--- portainer/enterprise-portainer-update.sh
+++ portainer/enterprise-portainer-update.sh
@@ -3,6 +3,7 @@
 # Version:      1.0.0
 # Tested on:    Debian 11 (Bullseye)
 #==============================================================================
+docker pull portainer/portainer-ee
 docker stop portainer
 docker rm portainer
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer \
